ffffa896e37ca5537484f4209889d4f9e2a2a9ee
Evgeniy Ivanov <p8635241@gmail.com>
2024-03-19 17:24:06 +0500

    new components main, flexwrapper

---
+++ src/components/FlexWrapper.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+type Props = {
+    direction?: string
+    justify?: string
+    align?: string
+}
+
+const FlexWrapper = styled.div`
+    display: flex;
+`
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.webp'
 export const Main = () => {
     return (
         <div>
@@ -8,7 +9,13 @@ export const Main = () => {
                 <h2>I am Devil within</h2>
                 <h1>A Web Developer.</h1>
             </div>
-            <img src="" alt=""/>
+            <StyledImage src={photo} alt=''/>
         </div>
     );
 };
+
+const StyledImage = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
