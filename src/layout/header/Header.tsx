cbb9e8cb662d3116ef2b1e9102537d405fb1fdd6
Evgeniy Ivanov <p8635241@gmail.com>
2024-03-19 16:56:43 +0500

    new components header, logo, main, menu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Logo} from "../../components/logo/Logo";
+import {Menu} from "../../components/menu/Menu";
 
 export const Header = () => {
     return (
-        <header>
-            <a href="">
-                <img src="" alt=""/>
-            </a>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="">Skills</a>
-                    </li>
-                    <li>
-                        <a href="">Works</a>
-                    </li>
-                    <li>
-                        <a href="">Testimony</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <StyledHeader>
+        <Logo/>
+        <Menu/>
+        </StyledHeader>
     );
 };
 
-const
+const StyledHeader = styled.header`
+    background-color: greenyellow;
+    display: flex;
+    justify-content: space-between;
+`
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <div>
+            <div>
+                <span>Hi there</span>
+                <span>I am Devil within</span>
+            </div>
+        </div>
+    );
+};
