29a035d6161f5d541c5b76b91f58ea1d7fbaf24f
Evgeniy Ivanov <p8635241@gmail.com>
2024-03-20 16:33:14 +0500

    new component contactc

---
+++ src/layout/sections/contacts/Contact.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+
+export const Contact = () => {
+    return (
+        <StyledContacts>
+        <SectionTitle>Contact</SectionTitle>
+        </StyledContacts>
+    );
+};
+
+
+const StyledContacts = styled.section`
+
+`
