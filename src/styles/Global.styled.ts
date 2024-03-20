3fbf1d024b93dbaac55a53925d5bc25b129e54c3
Evgeniy Ivanov <p8635241@gmail.com>
2024-03-20 23:48:07 +0500

    new component container

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -1,4 +1,5 @@
 import {createGlobalStyle} from "styled-components";
+import {theme} from "./Theme";
 
 
 export const GlobalStyle = createGlobalStyle`
@@ -15,6 +16,7 @@ export const GlobalStyle = createGlobalStyle`
         sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        color: ${theme.colors.font};
     }
     
     a {
@@ -28,5 +30,12 @@ export const GlobalStyle = createGlobalStyle`
     button {
         background-color: unset;
         border: unset;
+    }
+    
+    section:nth-of-type(odd) {
+        background-color: ${theme.colors.primaryBg};
+    }
+    section:nth-of-type(even) {
+        background-color: ${theme.colors.secondaryBg};
     }
 `
